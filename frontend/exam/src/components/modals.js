8f549426a2442a8402727fe462f4260d82f1dfe1
Siil777 <pavel.ivanov.thk@gmail.com>
2025-01-11 18:39:43 +0200

    feat: mui btn save

--- frontend/exam/src/components/modals.js
+++ frontend/exam/src/components/modals.js
@@ -1,4 +1,5 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import SecondaryBtn from './btnAdd';
 
 const Modal = ({ celldata, onClose, onSave }) => {
     const [day, setDay] = useState(celldata.day || '');
@@ -12,7 +13,7 @@ const Modal = ({ celldata, onClose, onSave }) => {
 
     const handleSave = (e) => {
         e.preventDefault();
-        const savedData = {day: celldata.day,date,time,place,examiner}
+        const savedData = { day: celldata.day, date, time, place, examiner }
         console.log('Saved data', savedData);
         onSave(savedData);
     }
@@ -20,38 +21,38 @@ const Modal = ({ celldata, onClose, onSave }) => {
 
 
     return (
-        <div className='modal-overlay custom-modal'>
+        <div className='modal-overlay custom-modal custom-modal-position'>
             <div className='modal-content custom-modal-height'>
                 <button className='close-button' onClick={onClose}>
                     ×
                 </button>
                 <h4>Edit information</h4>
-                <input 
-                type='date'
-                value={date}
-                onChange={(e)=>setDate(e.target.value)}
+                <input
+                    type='date'
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
                 />
-                 <input 
-                className='mt-1'
-                type='time'
-                value={time}
-                onChange={(e)=>setTime(e.target.value)}
+                <input
+                    className='mt-1'
+                    type='time'
+                    value={time}
+                    onChange={(e) => setTime(e.target.value)}
                 />
-                 <input 
-                className='mt-1'
-                type='text'
-                value={place}
-                onChange={(e)=>setPlace(e.target.value)}
+                <input
+                    className='mt-1'
+                    type='text'
+                    value={place}
+                    onChange={(e) => setPlace(e.target.value)}
                 />
-                 <input 
-                className='mt-1'
-                type='text'
-                value={examiner}
-                onChange={(e)=>setExaminer(e.target.value)}
+                <input
+                    className='mt-1'
+                    type='text'
+                    value={examiner}
+                    onChange={(e) => setExaminer(e.target.value)}
                 />
-                <button className='btn btn-outline-primary mt-2' onClick={handleSave}>
-                    Save changes
-                </button>
+                <div className='d-flex justify-content-center'>
+                    <SecondaryBtn onClick={handleSave} />
+                </div>
             </div>
         </div>
     );
